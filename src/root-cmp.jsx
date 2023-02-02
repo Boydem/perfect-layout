037373184050f48a6fbcd94eab55017f1a772138
Boydem <noyemdahan@gmail.com>
2023-02-02 20:48:35 +0200

    fixed home page navigation

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,10 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import { Home } from './pages/home'
 import './assets/styles/styles.scss'
 import { AppHeader } from './pages/app-common/app-header'
 import { AppFooter } from './pages/app-common/app-footer'
+import { useEffect } from 'react'
 
 export function App() {
+    const navigate = useNavigate()
+    useEffect(() => {
+        navigate('/')
+    }, [])
     return (
         <div className='app'>
             <AppHeader />
