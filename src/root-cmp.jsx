cf66ebe0a1a277451c32320c8b1f24d31009f6f1
Boydem <noyemdahan@gmail.com>
2023-02-06 15:56:33 +0200

    fixing ghpages

--- src/root-cmp.jsx
+++ src/root-cmp.jsx
@@ -1,22 +1,14 @@
-import { Route, Routes, useNavigate } from 'react-router-dom'
 import { Home } from './pages/home'
 import './assets/styles/styles.scss'
 import { AppHeader } from './pages/app-common/app-header'
 import { AppFooter } from './pages/app-common/app-footer'
-import { useEffect } from 'react'
 
 export function App() {
-    const navigate = useNavigate()
-    useEffect(() => {
-        navigate('/')
-    }, [])
     return (
         <div className='app'>
             <AppHeader />
             <main className='main-layout'>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                </Routes>
+                <Home />
             </main>
             <AppFooter />
         </div>
